482c6c43f8d4d3dc4313a59fca96361d07337544
kishor <kishor.ghanekar@yahoo.com>
2020-06-24 20:28:01 +0530

    changed source path

--- vpc/main.tf
+++ vpc/main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 module "network" {
-  source     = "/home/kishor/Downloads/dedvpc//modules/network"
+  source     = "..//modules/network"
   
   cidr_block = "10.0.0.0/16"
   tags       = { "kubeadm:cluster" = module.cluster.cluster_name }
 }
 
 module "cluster" {
-  source    = "/home/kishor/Downloads/dedvpc"
+  source    = "../"
   
   vpc_id    = module.network.vpc_id
   subnet_id = module.network.subnet_id
