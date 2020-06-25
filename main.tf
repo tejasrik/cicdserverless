cd4e61f738b3c80115996be9c4bb605e38c976ed
Kishor Ghanekar <48198031+kishorsg@users.noreply.github.com>
2020-06-25 21:39:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -244,9 +244,9 @@ resource "null_resource" "wait_for_bootstrap_to_finish" {
       break
     done
     EOF
-triggers = {
-    instance_ids = join(",", concat([aws_instance.master.id], aws_instance.workers[*].id))
   }
+  triggers = {
+    instance_ids = join(",", concat([aws_instance.master.id], aws_instance.workers[*].id))
   }
   
 }
