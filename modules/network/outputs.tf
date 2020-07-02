7c59440bdab71b8dc3a4301bce252f03211e06a4
Kishor Ghanekar <48198031+kishorsg@users.noreply.github.com>
2020-07-02 22:44:08 +0530

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,9 +1,7 @@
 output "vpc_id" {
   value       = aws_vpc.main.id
-  description = "ID of the created VPC."
 }
 
 output "subnet_id" {
   value       = aws_subnet.main.id
-  description = "ID of the created subnet."
 }
