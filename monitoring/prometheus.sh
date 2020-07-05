d4b6e38e4d35ba85accc414c915535a19542129d
Kishor Ghanekar <48198031+kishorsg@users.noreply.github.com>
2020-07-05 21:22:06 +0530

    Update prometheus.sh

--- monitoring/prometheus.sh
+++ monitoring/prometheus.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
-kubectl create namespace monitoring
-kubectl create -f clusterRole.yaml
-kubectl create -f config-map.yaml
-kubectl create  -f prometheus-deployment.yaml 
-kubectl create -f prometheus-service.yaml --namespace=monitoring
+kubectl create namespace monitoring --kubeconfig /home/ubuntu/admin.conf
+kubectl create -f clusterRole.yaml --kubeconfig /home/ubuntu/admin.conf
+kubectl create -f config-map.yaml --kubeconfig /home/ubuntu/admin.conf
+kubectl create  -f prometheus-deployment.yaml --kubeconfig /home/ubuntu/admin.conf
+kubectl create -f prometheus-service.yaml --namespace=monitoring --kubeconfig /home/ubuntu/admin.conf
 
   
