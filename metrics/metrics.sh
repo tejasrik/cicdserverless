35791854dd34cd8cc040ffcd316ca83841d898df
Kishor Ghanekar <48198031+kishorsg@users.noreply.github.com>
2020-07-05 21:20:55 +0530

    Update metrics.sh

--- metrics/metrics.sh
+++ metrics/metrics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-kubectl create -f cluster-role-binding.yaml
-kubectl create -f cluster-role.yaml                 
-kubectl create -f deployment.yaml
-kubectl create -f service-account.yaml
-kubectl create -f service.yaml
+kubectl create -f cluster-role-binding.yaml --kubeconfig /home/ubuntu/admin.conf
+kubectl create -f cluster-role.yaml --kubeconfig /home/ubuntu/admin.conf                
+kubectl create -f deployment.yaml --kubeconfig /home/ubuntu/admin.conf
+kubectl create -f service-account.yaml --kubeconfig /home/ubuntu/admin.conf
+kubectl create -f service.yaml --kubeconfig /home/ubuntu/admin.conf
